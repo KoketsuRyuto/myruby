a8d714128dfb9bf353c2ee74a2c98bdfa89384c0
纐纈琉斗 <kouketsuryuto@kouketsuryutonoMacBook-Air.local>
2023-04-21 00:07:40 +0900

    [Add]myruby

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  root to: "homes#top"
-  get "homes/about" => "homes#about",as: "about"
+  root to: 'homes#top'
+  get 'homes/about' => 'homes#about', as: 'about'
+
+  resources :post_images, only: %i[new create show destroy index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
